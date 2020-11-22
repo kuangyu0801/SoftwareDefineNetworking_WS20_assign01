5a2864f511699d89681b014331d260ece7cac04b
kuangyu0801 <57182723+kuangyu0801@users.noreply.github.com>
2020-11-22 22:30:47 +0100

    fix: task13.sh ping pass for h1, h2 adding additional flow for arp

--- remote_sshfs_ex1/task13.sh
+++ remote_sshfs_ex1/task13.sh
@@ -4,7 +4,7 @@ curl http://localhost:8080/wm/staticentrypusher/clear/all/json
 
 # flood arp request
 
-
+# IP forwarding
 # S1
 curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"s1-src-h2-dst-h3","eth_type":"0x0800", "eth_src":"00:00:00:00:00:02", "eth_dst":"00:00:00:00:00:03", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
 curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"s1-src-h2-dst-h4","eth_type":"0x0800", "eth_src":"00:00:00:00:00:02", "eth_dst":"00:00:00:00:00:04", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
@@ -12,9 +12,19 @@ curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"s1-src-h3-dst-h2",
 curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"s1-src-h4-dst-h2","eth_type":"0x0800", "eth_src":"00:00:00:00:00:04", "eth_dst":"00:00:00:00:00:02", "active":"true", "actions":"output=3"}' http://localhost:8080/wm/staticentrypusher/json
 
 # S2
-curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h2-dst-h3","eth_type":"0x0800", "eth_src":"00:00:00:00:00:02", "eth_dst":"00:00:00:00:00:03", "active":"true", "actions":"output=3"}' http://localhost:8080/wm/staticentrypusher/json
-curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h2-dst-h4","eth_type":"0x0800", "eth_src":"00:00:00:00:00:02", "eth_dst":"00:00:00:00:00:04", "active":"true", "actions":"output=2"}' http://localhost:8080/wm/staticentrypusher/json
-curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h3-dst-h2","eth_type":"0x0800", "eth_src":"00:00:00:00:00:03", "eth_dst":"00:00:00:00:00:02", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
-curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h4-dst-h2","eth_type":"0x0800", "eth_src":"00:00:00:00:00:04", "eth_dst":"00:00:00:00:00:02", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h2-dst-h3", "eth_src":"00:00:00:00:00:02", "eth_dst":"00:00:00:00:00:03", "active":"true", "actions":"output=3"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h2-dst-h4", "eth_src":"00:00:00:00:00:02", "eth_dst":"00:00:00:00:00:04", "active":"true", "actions":"output=2"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h3-dst-h2", "eth_src":"00:00:00:00:00:03", "eth_dst":"00:00:00:00:00:02", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"s2-src-h4-dst-h2", "eth_src":"00:00:00:00:00:04", "eth_dst":"00:00:00:00:00:02", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
 
 
+# ARP forwarding
+
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"arp-s1-src-h2", "eth_type":"0x0806", "arp_sha":"00:00:00:00:00:02", "active":"true", "actions":"output=1"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"arp-s1-src-h3", "eth_type":"0x0806", "arp_sha":"00:00:00:00:00:03", "active":"true", "actions":"output=3"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:01", "name":"arp-s1-src-h4", "eth_type":"0x0806", "arp_sha":"00:00:00:00:00:04", "active":"true", "actions":"output=3"}' http://localhost:8080/wm/staticentrypusher/json
+
+# S2
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"arp-s2-src-h2-dst-h3", "eth_type":"0x0806", "arp_sha":"00:00:00:00:00:02", "active":"true", "actions":"output=3,output=2"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"arp-s2-src-h3-dst-h2", "eth_type":"0x0806", "arp_sha":"00:00:00:00:00:03", "active":"true", "actions":"output=1,output=2"}' http://localhost:8080/wm/staticentrypusher/json
+curl -X POST -d '{"switch":"00:00:00:00:00:00:00:02", "name":"arp-s2-src-h4-dst-h2", "eth_type":"0x0806", "arp_sha":"00:00:00:00:00:04", "active":"true", "actions":"output=1,output=3"}' http://localhost:8080/wm/staticentrypusher/json
